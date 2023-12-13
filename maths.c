92249b3c6a5197d8b0766723e9b4751771de58a4
maxist101 <netochukwu26@gmail.com>
2023-12-13 15:48:14 -0800

    add

---
+++ maths.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * my_add - Adds the top two elements of the stack
+ * @nodepointer: nodepointer
+ * @line_number: Int Line
+ */
+void my_add(stack_t **nodepointer, unsigned int line_number)
+{
+	stack_t *initial;
+	stack_t *latter;
+	int add;
+
+	if (*nodepointer == NULL || (*nodepointer)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fclose(env.file);
+		free(env.payload);
+		free_stack(*nodepointer);
+		exit(EXIT_FAILURE);
+	}
+
+	initial = *nodepointer;
+	latter = initial->next;
+	add = initial->n + latter->n;
+
+	latter->n = add;
+	*nodepointer = latter;
+	free(initial);
+}
---
+++ queue.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+ * my_stack - prints top
+ * @nodepointer: nodepointer
+ * @line_number: Int line
+ * Return: null
+ */
+void my_stack(stack_t **nodepointer, unsigned int line_number)
+{
+	(void)nodepointer;
+	(void)line_number;
+	env.nofi = 0;
+}
+/**
+ * my_queue - prints top
+ * @nodepointer: nodepointer
+ * @line_number: Int line
+ * Return: null
+ */
+void my_queue(stack_t **nodepointer, unsigned int line_number)
+{
+	(void)nodepointer;
+	(void)line_number;
+	env.nofi = 1;
+}
